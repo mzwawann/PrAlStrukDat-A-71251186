987ee9e22628ab7699a8b7cc11852c05871a6380
kurniawannn <kurniawanfirdaus2806@gmail.com>
2026-09-03 14:47:11 +0700

    71251186

---
+++ pola_sakit_kepala.py
@@ -0,0 +1,11 @@
+def pola_sakit_kepala(panjang, lebar):
+    pass
+
+print("no 1. (pola 7,7)")
+pola_sakit_kepala(7, 7)
+print()
+print("no 2. (Pola 4, 4)")
+pola_sakit_kepala(4, 4)
+print()
+print("no 3. (Pola -15, 15)")
+pola_sakit_kepala(-15, 15)
