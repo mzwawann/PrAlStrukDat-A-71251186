241e349ca888dfadab24a48869ce4996054dd3aa
kurniawannn <kurniawanfirdaus2806@gmail.com>
2026-09-03 15:46:29 +0700

    71251186

--- pola_sakit_kepala.py
+++ pola_sakit_kepala.py
@@ -1,5 +1,13 @@
 def pola_sakit_kepala(panjang, lebar):
-    pass
+    panjang = (panjang)
+    lebar = (lebar)
+
+    input(panjang, lebar)
+
+    for i in range (number + 1):
+        spasi = "  " * (number - baris)
+        naik = " ".join(str(i) for i in range(1, baris + 1))
+        turun = " ".join(str(i) for i in range(baris - 1, 0, -1))
 
 print("no 1. (pola 7,7)")
 pola_sakit_kepala(7, 7)
