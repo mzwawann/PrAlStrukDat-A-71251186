a09b2f42390b4153e85e7677bc43df3be69347e8
kurniawannn <kurniawanfirdaus2806@gmail.com>
2026-09-10 15:33:39 +0700

    71251186

---
+++ UG1-AnalisisAlgoritma-Rekursif/Soal#1/SortNim.py
@@ -0,0 +1,66 @@
+def InsertRecursive(sorted_array, current_value, current_length, is_odd):
+    # TODO 1: Implementasikan base case, logika komparasi pengurutan sesuai NIM, 
+    # dan pemanggilan rekursi fungsi insert.
+    
+    if current_length == 0:
+        return [current_value]
+
+    # Ambil elemen terakhir
+    last = sorted_array[current_length - 1]
+
+    # Jika tipe GANJIL → ascending
+    if is_odd:
+        if current_value >= last:
+            return sorted_array[:current_length] + [current_value]
+        else:
+            return InsertRecursive(sorted_array[:current_length-1], current_value, current_length-1, is_odd) + [last]
+    # Jika tipe GENAP → descending
+    else:
+        if current_value <= last:
+            return sorted_array[:current_length] + [current_value]
+        else:
+            return InsertRecursive(sorted_array[:current_length-1], current_value, current_length-1, is_odd) + [last]
+
+
+def RecursiveFilterSort(data_array, current_length, is_odd):
+    # TODO 2: Implementasikan base case, pemecahan rekursif, dan filter kondisional 
+    # untuk memanggil fungsi InsertRecursive sesuai paritas NIM.
+     # Base case
+    if current_length == 0:
+        return []
+
+    # Ambil elemen terakhir
+    current_value = data_array[current_length - 1]
+
+    # Rekursi untuk sisa array
+    result = RecursiveFilterSort(data_array, current_length - 1, is_odd)
+
+    # Filter sesuai aturan NIM
+    if is_odd and current_value % 2 == 1:
+        return InsertRecursive(result, current_value, len(result), is_odd)
+    elif not is_odd and current_value % 2 == 0:
+        return InsertRecursive(result, current_value, len(result), is_odd)
+    else:
+        return result
+
+
+# Ganti Dengan NIM Anda
+# Contoh, NIM_MAHASISWA = "71230994" -> nanti outputnya [4, 2, 0] 
+NIM_MAHASISWA = "71251186"
+
+if NIM_MAHASISWA != "":
+    raw_data = [int(digit) for digit in NIM_MAHASISWA]
+    data_length = len(raw_data)
+    
+     # Cek ganjil/genap
+    is_odd = int(NIM_MAHASISWA[-1]) % 2 == 1
+    tipe = "GANJIL (Ascending)" if is_odd else "GENAP (Descending)"
+    
+    final_result = RecursiveFilterSort(raw_data, data_length, is_odd)
+
+    # TODO 3: cetak hasil akhir sesuai format yang diminta.
+    print("===== FILTER & SORT NIM =====")
+    print("NIM Mahasiswa  :", NIM_MAHASISWA)
+    print("Tipe           :", tipe)
+    print("Data Digit Awal:", raw_data)
+    print("Hasil Akhir    :", final_result)
---
+++ UG1-AnalisisAlgoritma-Rekursif/Soal#2/SistemKopdes.py
@@ -0,0 +1,62 @@
+# import ini jangan dihapus/diedit yak
+import random
+
+def totalPenjualan(data, n):
+    if n == 0:  #saat tidak ada data yang tersisa, fungsi berhenti dan mengembalikan 0
+        return 0
+    return data[n-1][1] + totalPenjualan(data, n-1) #data[n-1][1] = mengambil nilai penjualan dari elemen terakhir, n adalah jumlah elemen yang sedang diproses
+
+# Fungsi rekursif untuk mencari penjualan tertinggi
+def penjualanTertinggi(data, n):
+    if n == 1:  # base case
+        return data[0]
+    
+    tertinggi_sebelum = penjualanTertinggi(data, n-1)
+    if data[n-1][1] > tertinggi_sebelum[1]:
+        return data[n-1]
+    else:
+        return tertinggi_sebelum
+
+# Fungsi iteratif untuk menghitung jumlah barang di atas rata-rata
+def diAtasRataRata(penjualan, rataRata):
+    count = 0
+    for jumlah in penjualan.values():
+        if jumlah > rataRata:
+            count += 1
+    return count
+
+# Program Utama - Jangan dihapus/diedit yak
+angka = int(input("NIM: "))
+random.seed(angka)
+
+barang = [
+    "Beras",
+    "Minyak",
+    "Gula",
+    "Telur",
+    "Kopi",
+    "Teh"
+]
+
+penjualan = {}
+
+for namaBarang in barang:
+    penjualan[namaBarang] = random.randint(100, 500)
+
+data = list(penjualan.items())
+n = len(data)
+
+print("\n===== Data Penjualan =====")
+for namaBarang, jumlah in penjualan.items():
+    print(namaBarang, ":", jumlah)
+
+total = totalPenjualan(data, n)
+tertinggi = penjualanTertinggi(data, n)
+rataRata = total / n
+jumlahDiAtasRataRata = diAtasRataRata(penjualan, rataRata)
+
+print("\n===== Hasil Analisis =====")
+print("Total penjualan        :", total)
+print("Penjualan tertinggi    :", tertinggi[0], "(", tertinggi[1], ")")
+print("Rata-rata penjualan    :", round(rataRata, 2))
+print("Di atas rata-rata      :", jumlahDiAtasRataRata, "barang")
