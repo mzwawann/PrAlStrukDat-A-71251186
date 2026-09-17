3c77ea0316c650274aaa84c28329b0360392cf62
kurniawannn <kurniawanfirdaus2806@gmail.com>
2026-09-17 15:47:11 +0700

    71251186

--- ug2/soal2/sortMahasiswa.py
+++ ug2/soal2/sortMahasiswa.py
@@ -18,21 +18,18 @@ def sort_by(data: list=data, index: str="nim",rev = False):
     # Kerjakan disini
     kolom = maps[index]
 
-    # Bubble Sort
     n = len(data)
 
     for i in range(n - 1):
         for j in range(n - 1 - i):
 
-            # Ascending
             if not rev:
                 if data[j][kolom] > data[j + 1][kolom]:
-                    data[j], data[j + 1] = data[j + 1], data[j]
+                    data[j], data[j + 1] = data[j + 1], data[j] 
 
-            # Descending
             else:
                 if data[j][kolom] < data[j + 1][kolom]:
-                    data[j], data[j + 1] = data[j + 1], data[j]
+                    data[j], data[j + 1] = data[j + 1], data[j] 
     
     # Jangan Dihapus
     show_data(data)
